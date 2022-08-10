e220f343a23579c1c6076a67194c071b0d3b5411
Aurélio Nogueira Mesquita <aurelio.mesquita@interaduaneira.com.br>
2022-08-09 21:22:47 -0300

    Criando os componentes necessarios do prisma

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,10 @@
+import { PrismaModule } from './../prisma/prisma.module';
 import { AuthController } from './auth.controller';
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [AuthController],
   providers: [AuthService],
 })
